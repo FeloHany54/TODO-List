c715aff10985f9555a7676d6693c961585999588
FeloHany54 <felohany54@gmail.com>
2025-06-16 04:23:40 +0300

    ho

--- lib/Screens/home-screen.dart
+++ lib/Screens/home-screen.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key, this.counter});
+  int? counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: IconButton(
-        color: Color(0xff8875FF),
+        style: ButtonStyle(
+          backgroundColor: WidgetStateColor.resolveWith((
+            Set<WidgetState> states,
+          ) {
+            return Color(0xff8875FF);
+          }),
+        ),
+
         onPressed: () {},
         icon: Icon(Icons.add, color: Colors.black),
       ),
       backgroundColor: Color(0xffD9D9D9),
       appBar: AppBar(
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.black,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.person, color: Color(0xffD9D9D9)),
+              ),
+            ),
+          ),
+        ],
         backgroundColor: Color(0xffD9D9D9),
         title: Text(
           "June 16,2025",
@@ -25,26 +46,50 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Expanded(
-          child: Column(
-            children: [
-              Text(
-                "Tasks",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w700,
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Tasks",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Image.asset(
-                "assets/let'sGo.png",
-                fit: BoxFit.cover,
-                height: 200,
-                width: 200,
-              ),
-            ],
-          ),
+              ],
+            ),
+            SizedBox(height: 100),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/let'sGo.png",
+                  fit: BoxFit.cover,
+                  height: 200,
+                  width: 200,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "To Add Your Task Press "
+                  "  +  "
+                  " ",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
